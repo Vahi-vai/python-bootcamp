a0536d0cd519ad9878e042071f5fd6c953f09ab9
R VAISHNAV <85779920+Vahi-vai@users.noreply.github.com>
2021-06-13 09:39:46 +0530

    Add files via upload

---
+++ task1/maximum of two numbers.py
@@ -0,0 +1,8 @@
+import math
+
+def maxi(num1,num2):
+    return max(num1, num2)
+
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+print(maxi(num1,num2))
