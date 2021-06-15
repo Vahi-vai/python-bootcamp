1584b53e22b5306d5c8f9649565f5bd95ee85c4d
R VAISHNAV <85779920+Vahi-vai@users.noreply.github.com>
2021-06-15 16:46:42 +0530

    Add files via upload

---
+++ TASK4-  all exercises.py
@@ -0,0 +1,20 @@
+# exercise 1
+a = 5
+b = 5
+c = 5
+print(a/10)
+print(b*50)
+print(c+60)
+# exercise 2
+d = 'great' # original string
+e = list(d)
+e[4] = 'g'
+str1 = ""
+print(str1.join(e)) # prints new string
+# exercise 3
+f = 5          #int datatype
+g = 6.4        #float datatype
+g = int(g)     #Converting to int
+f = float(f)   #Converting to float
+print (f)
+print(g)
