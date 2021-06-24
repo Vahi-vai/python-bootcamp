85da19268e40e2bfc395d1dabd5a0ff16406e31d
R VAISHNAV <85779920+Vahi-vai@users.noreply.github.com>
2021-06-24 17:59:16 +0530

    Add files via upload

---
+++ task11 - all exercises.py
@@ -0,0 +1,13 @@
+#Exercise 1
+import listtt
+listtt.list1()
+# Exercise 2
+"""successfully installed pandas"""
+#exercise 3
+import random
+n = int(input("Enter the number of random numbers you want"))
+for i in range( 0 , n):
+    print(random.randint(0,100))
+#Exercise 4
+import sys
+print(sys.path)
